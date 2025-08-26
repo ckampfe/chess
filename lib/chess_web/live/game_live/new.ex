821dea8a739f76577ae5671c0336f06642306dac
Clark Kampfe <clark.kampfe@gmail.com>
2025-08-25 22:06:41 -0500

    use dialog element for new games

--- lib/chess_web/live/game_live/new.ex
+++ lib/chess_web/live/game_live/new.ex
@@ -6,10 +6,14 @@ defmodule ChessWeb.GameLive.New do
     ~H"""
     <div>
       Play as:
-      <div>
+      <dialog
+        class="m-auto mt-8 rounded-xl bg-white p-6 shadow-3xl backdrop:bg-black/50 backdrop:backdrop-blur-md"
+        phx-mounted={JS.dispatch("chess:show-modal")}
+      >
+        <p>Play as:</p>
         <.button phx-click="new-game" phx-value-playing_as="black">Black</.button>
         <.button phx-click="new-game" phx-value-playing_as="white">White</.button>
-      </div>
+      </dialog>
     </div>
     """
   end
